187df59e718c9896aec601b68ffac1f5d67a9d29
Muhammad Iqbal <iqbalmineraltown@gmail.com>
2022-07-12 15:18:32 +0700

    counter riverpod with widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,33 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_eos_playground/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('update the UI when incrementing the state', (tester) async {
+    await tester.pumpWidget(const ProviderScope(child: MyApp()));
 
-    // Verify that our counter starts at 0.
+    // The default value is `0`, as declared in our provider
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Increment the state and re-render
+    await tester.tap(find.byType(FloatingActionButton));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
+    // The state have properly incremented
     expect(find.text('1'), findsOneWidget);
+    expect(find.text('0'), findsNothing);
+  });
+
+  testWidgets('the counter state is not shared between tests', (tester) async {
+    await tester.pumpWidget(const ProviderScope(child: MyApp()));
+
+    // The state is `0` once again, with no tearDown/setUp needed
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
   });
 }
